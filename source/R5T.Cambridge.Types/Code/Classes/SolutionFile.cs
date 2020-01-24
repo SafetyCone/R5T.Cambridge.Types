81a0e306395f17c26a1da69e179d403eda3aebe8
MinexAutomation <minex.automation@gmail.com>
2020-01-24 13:30:36 -0800

    Added basic structure to the SolutionFile implmentation (ContentLines: List<String> for now).

--- source/R5T.Cambridge.Types/Code/Classes/SolutionFile.cs
+++ source/R5T.Cambridge.Types/Code/Classes/SolutionFile.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
 namespace R5T.Cambridge.Types
 {
     public class SolutionFile
     {
+        public List<string> ContentLines { get; } = new List<string>();
     }
 }
