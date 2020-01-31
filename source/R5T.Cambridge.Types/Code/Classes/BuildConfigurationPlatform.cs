2debb9923d0437d32eefa7818d055f002c041594
MinexAutomation <minex.automation@gmail.com>
2020-01-31 13:52:29 -0800

    Added BuildConfigurationPlatform static values for all build configuration-platform values.

--- source/R5T.Cambridge.Types/Code/Classes/BuildConfigurationPlatform.cs
+++ source/R5T.Cambridge.Types/Code/Classes/BuildConfigurationPlatform.cs
@@ -5,6 +5,14 @@ namespace R5T.Cambridge.Types
 {
     public class BuildConfigurationPlatform
     {
+        public static readonly BuildConfigurationPlatform DebugAnyCPU = new BuildConfigurationPlatform { BuildConfiguration = BuildConfiguration.Debug, Platform = Platform.AnyCPU };
+        public static readonly BuildConfigurationPlatform DebugX64 = new BuildConfigurationPlatform { BuildConfiguration = BuildConfiguration.Debug, Platform = Platform.x64 };
+        public static readonly BuildConfigurationPlatform DebugX86 = new BuildConfigurationPlatform { BuildConfiguration = BuildConfiguration.Debug, Platform = Platform.x86 };
+        public static readonly BuildConfigurationPlatform ReleaseAnyCPU = new BuildConfigurationPlatform { BuildConfiguration = BuildConfiguration.Release, Platform = Platform.AnyCPU };
+        public static readonly BuildConfigurationPlatform ReleaseX64 = new BuildConfigurationPlatform { BuildConfiguration = BuildConfiguration.Release, Platform = Platform.x64 };
+        public static readonly BuildConfigurationPlatform ReleaseX86 = new BuildConfigurationPlatform { BuildConfiguration = BuildConfiguration.Release, Platform = Platform.x86 };
+
+
         public BuildConfiguration BuildConfiguration { get; set; }
         public Platform Platform { get; set; }
     }
