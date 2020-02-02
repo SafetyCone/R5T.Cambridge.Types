7a275708405e8f2308d9d6b559cc03abc27551a1
MinexAutomation <minex.automation@gmail.com>
2020-02-01 19:16:21 -0800

    Added New() static constructor method.

--- source/R5T.Cambridge.Types/Code/Global Sections/Classes/ExtensibilityGlobalsGlobalSection.cs
+++ source/R5T.Cambridge.Types/Code/Global Sections/Classes/ExtensibilityGlobalsGlobalSection.cs
@@ -7,6 +7,23 @@ namespace R5T.Cambridge.Types
     {
         public const string GlobalSectionName = "ExtensibilityGlobals";
 
+        #region Static
+
+        /// <summary>
+        /// Creates a new <see cref="ExtensibilityGlobalsGlobalSection"/> with the <see cref="ExtensibilityGlobalsGlobalSection.GlobalSectionName"/> and <see cref="PreOrPostSolution.PostSolution"/>.
+        /// </summary>
+        public static ExtensibilityGlobalsGlobalSection New()
+        {
+            var output = new ExtensibilityGlobalsGlobalSection
+            {
+                Name = ExtensibilityGlobalsGlobalSection.GlobalSectionName,
+                PreOrPostSolution = PreOrPostSolution.PostSolution,
+            };
+            return output;
+        }
+
+        #endregion
+
 
         public Guid SolutionGuid { get; set; }
     }
--- source/R5T.Cambridge.Types/Code/Global Sections/Classes/SolutionPropertiesGlobalSection.cs
+++ source/R5T.Cambridge.Types/Code/Global Sections/Classes/SolutionPropertiesGlobalSection.cs
@@ -8,6 +8,24 @@ namespace R5T.Cambridge.Types
         public const string GlobalSectionName = "SolutionProperties";
 
 
+        #region Static
+
+        /// <summary>
+        /// Creates a new <see cref="SolutionPropertiesGlobalSection"/> with the <see cref="SolutionPropertiesGlobalSection.GlobalSectionName"/> and <see cref="PreOrPostSolution.PreSolution"/>.
+        /// </summary>
+        public static SolutionPropertiesGlobalSection New()
+        {
+            var output = new SolutionPropertiesGlobalSection
+            {
+                Name = SolutionPropertiesGlobalSection.GlobalSectionName,
+                PreOrPostSolution = PreOrPostSolution.PreSolution,
+            };
+            return output;
+        }
+
+        #endregion
+
+
         public bool HideSolutionNode { get; set; }
     }
 }
