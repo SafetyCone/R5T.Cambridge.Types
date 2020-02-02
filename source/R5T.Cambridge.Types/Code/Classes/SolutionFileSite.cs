2525d862e217b428e9720b35064a113b7f60e53f
MinexAutomation <minex.automation@gmail.com>
2020-02-02 01:21:56 -0800

    Added SolutionFileSite class type.

---
+++ source/R5T.Cambridge.Types/Code/Classes/SolutionFileSite.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Cambridge.Types
+{
+    public class SolutionFileSite
+    {
+        public SolutionFile SolutionFile { get; set; }
+        public string Path { get; set; }
+    }
+}
