0d843661fbbf08385aec30901d42f46a3bc7c1ff
MinexAutomation <minex.automation@gmail.com>
2020-02-07 00:28:01 -0800

    Added Constants.

---
+++ source/R5T.Cambridge.Types/Code/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Cambridge.Types
+{
+    public static class Constants
+    {
+        public static readonly Guid SolutionFolderProjectTypeGUID = Guid.Parse("2150E333-8FDC-42A3-9474-1A3956D46DE8");
+
+        //public static readonly Guid NetStandardLibraryProjectTypeGUID = Guid.Parse("9A19103F-16F7-4668-BE54-9A1E7A4F7556");
+    }
+}
