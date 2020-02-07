d0634d53f885f18f9726ef594eaa39538b9f7bd7
MinexAutomation <minex.automation@gmail.com>
2020-02-07 00:29:06 -0800

    Added full file path solution file project file reference type.

---
+++ source/R5T.Cambridge.Types/Code/Classes/SolutionFileProjectFileReference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Cambridge.Types
+{
+    public class SolutionFileProjectFileReference
+    {
+        public Guid ProjectTypeGUID { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectFilePathValue { get; set; }
+        public Guid ProjectGUID { get; set; }
+    }
+}
