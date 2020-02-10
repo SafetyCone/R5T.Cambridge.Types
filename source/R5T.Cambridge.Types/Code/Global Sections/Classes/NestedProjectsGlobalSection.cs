a7099c42fd4ee6f70a4197be8bab463201c8dffc
MinexAutomation <minex.automation@gmail.com>
2020-02-10 11:09:39 -0800

    Changed name of NestedProjectsGlobalSection.

---
+++ source/R5T.Cambridge.Types/Code/Global Sections/Classes/NestedProjectsGlobalSection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Cambridge.Types
+{
+    public class NestedProjectsGlobalSection : SolutionFileGlobalSectionBase
+    {
+        public const string GlobalSectionName = "NestedProjects";
+
+
+        #region Static
+
+        /// <summary>
+        /// Creates a new <see cref="NestedProjectsGlobalSection"/> with the <see cref="NestedProjectsGlobalSection.GlobalSectionName"/> and <see cref="PreOrPostSolution.PreSolution"/>.
+        /// </summary>
+        public static NestedProjectsGlobalSection New()
+        {
+            var output = new NestedProjectsGlobalSection
+            {
+                Name = NestedProjectsGlobalSection.GlobalSectionName,
+                PreOrPostSolution = PreOrPostSolution.PreSolution,
+            };
+            return output;
+        }
+
+        #endregion
+
+
+        public List<ProjectNesting> ProjectNestings { get; } = new List<ProjectNesting>();
+    }
+}
